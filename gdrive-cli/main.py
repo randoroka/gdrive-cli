81560d8fffb20f3fbc732accfe62152b12f0c988
‘Mar’ <jmaroka854@gmail.com>
2024-08-04 01:23:48 +0100

    moved files

--- gdrive-cli/main.py
+++ gdrive-cli/main.py
@@ -1,7 +1,8 @@
 import click
-from auth import login, logout
+import sys
 from list_content import list, list_dir, list_local
 from search_content import search
+from auth import login, logout
 from modify_local import rename, change_all
 from download import download_file
 
@@ -19,6 +20,8 @@ cli.add_command(list_local)
 cli.add_command(rename)
 cli.add_command(change_all)
 cli.add_command(download_file)
+
+
 
 if __name__ == "__main__":
     cli()
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gdrive',
     version='0.1.0',
+    packages=find_packages(),
     py_modules=['main'],
     install_requires=[
         'Click',
