e97ba07eb088222140c03605de5c2dfe8f907891
randoroka <jmaroka854@gmail.com>
2024-08-13 03:15:19 +0100

    shareable link

--- gdrive_cli/modify_drive.py
+++ gdrive_cli/modify_drive.py
@@ -12,8 +12,41 @@ def delete_file(file_id):
 
     try:
         service.files().delete(fileId=file_id).execute()
-        click.echo(click.style("Error: ", bold=True) + f"File with ID '{file_id}' deleted.")
+        click.echo(f"File with ID '{file_id}' deleted.")
     except HttpError as error:
         click.echo(click.style("Error: ", bold=True) + f"An error occurred: {error}")
     except Exception as e:
+        click.echo(click.style("Error: ", bold=True) + f"{e}")
+        
+# makes a link of folder in drive to share 
+@click.command('link', short_help='deletes files')
+@click.argument('folder_id', required=False, default=None)         
+def shareable_link(folder_id):
+    try:
+        # Load credentials
+        creds = Credentials.from_authorized_user_file("../token.json")
+        service = build('drive', 'v3', credentials=creds)
+        
+        user_permission = {
+            'type': 'anyone',  
+            'role': 'reader'   
+        }
+        
+        # Set the permission to the folder
+        service.permissions().create(
+            fileId=folder_id,
+            body=user_permission,
+            fields='id'
+        ).execute()
+
+        # Retrieve the shareable link
+        folder = service.files().get(fileId=folder_id, fields='webViewLink').execute()
+        click.echo(f"URL for the folder: {folder.get('webViewLink')}")
+    
+    except HttpError as error:
+        # Handles errors related to the Google Drive API
+        click.echo(click.style("Error: ", bold=True) + f"{error}")
+
+    except Exception as e:
+        # Handles all other exceptions
         click.echo(click.style("Error: ", bold=True) + f"{e}")
